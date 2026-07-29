9980b55113c6a30fe628504b7e1004c4a3becffe
ManoharGurrampati <manohargurram04@gmail.com>
2026-07-29 16:29:02 +0530

    "Commit at 27/07/26 via eclipse"

--- labsessions/src/com/fundamentals/Student1.java
+++ labsessions/src/com/fundamentals/Student1.java
@@ -14,35 +14,53 @@ public class Student1 {
 	long stdPhoneNum;
 	String stdAddress;
 	
-	public void setDetails() {
-		System.out.println("Enter Student Details");
-		System.out.println("Enter ID");
-		stdID = s.nextInt();
-		System.out.println("Enter Name");
-		stdName = s.next();
-		System.out.println("Enter degree");
-		degree = s.next();
-		System.out.println("Enter phone number");
-		stdPhoneNum = s.nextLong();
-		System.out.println("Enter Address");
-		stdAddress = s.next();
-	}
 	
+	public void setStdID() {
+		System.out.println("Enter  Studnet Id");
+		stdID = s.nextInt();
+		s.nextLine();
+	}
+
+	public void setStdName() {
+		System.out.println("Enter Student Name");
+		stdName = s.nextLine();
+	}
+
+	public void setDegree() {
+		System.out.println("Enter Student Degree");
+		degree = s.next();
+	}
+
+	public void setStdPhoneNum() {
+		System.out.println("Enter Student Phone Number");
+		stdPhoneNum = s.nextLong();
+		s.nextLine();
+	}
+
+	public void setStdAddress() {
+		System.out.println("Enter Student Address");
+		stdAddress = s.nextLine();
+	}
+
 	void displayDetails() {
 		System.out.println("===================================");
 		System.out.println("Student Details");
 		System.out.println("College Name : "+collegeName);
 		System.out.println("Std ID : "+stdID);
-		System.out.println("Std Name"+stdName);
-		System.out.println("Std Degree"+degree);
-		System.out.println("Std Phone Number : "+stdPhoneNum);
-		System.out.println("Std Address : "+stdAddress);
+		System.out.println("Std Name :"+stdName);
+		System.out.println("Std Degree :"+degree);
+		System.out.println("Std Phone Number :"+stdPhoneNum);
+		System.out.println("Std Address :"+stdAddress);
 		System.out.println("===================================");
 	}
 	
 	public static void main(String[] args) {
 		Student1 obj = new Student1();
-		obj.setDetails();
+		obj.setStdID();
+		obj.setStdName();
+		obj.setDegree();
+		obj.setStdPhoneNum();
+		obj.setStdAddress();
 		obj.displayDetails();
 	}
 
