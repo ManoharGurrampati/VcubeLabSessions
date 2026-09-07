6932434e430ae9529b70bb8a4b0759762a44243d
ManoharGurrampati <manohargurram04@gmail.com>
2026-09-07 09:53:56 +0530

    Committed on 07/09/2026

---
+++ labsessions/src/com/mediumleveltasks/OnlineVotingSystem.java
@@ -0,0 +1,60 @@
+package com.mediumleveltasks;
+
+import java.util.Scanner;
+
+public class OnlineVotingSystem {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void castYourVote() {
+		System.out.println("| ====> Welcome to Online Voting System <==== |");
+		System.out.println("\nEnter Your Age: ");
+		byte age = sc.nextByte();
+//		Age Validation
+		if (age < 18) {
+			System.out.println("Your Not Eligible For Voting. (UnderAge)");
+		} else {
+			System.out.println("Enter Your Gender: ");
+			char gender = sc.next().toUpperCase().charAt(0);
+//			Gender Validation
+			if (gender == 'M' || gender == 'F') {
+				System.out.println("Your Eligible To Cast Your Vote Now.");
+				System.out.println("Enter Your VoterID: ");
+				int voterID = sc.nextInt();
+				boolean isVoted = true;
+				do {
+					System.out.println("-----> Candidates <------");
+					System.out.println("1. Manohar G");
+					System.out.println("2. Sai V");
+					System.out.println("3. Yogesh B");
+					System.out.println("4. Abhinav E");
+					System.out.println("--------------------------");
+					System.out.println("Select The Profile To Cast Your Vote !");
+					int vote = sc.nextInt();
+					switch (vote) {
+					case 1 -> System.out.println("Your Vote Is Captured For Manohar G | With VoterID " + voterID);
+					case 2 -> System.out.println("Your Vote Is Captured For Sai V | With VoterID " + voterID);
+					case 3 -> System.out.println("Your Vote Is Captured For Yogesh B | With VoterID " + voterID);
+					case 4 -> System.out.println("Your Vote Is Captured For Abhinav E | With VoterID " + voterID);
+					default -> {
+						if (vote <= 0) {
+							System.out.println("Please Vote To A Proper Candidate !");
+							isVoted = true;
+						} else {
+							System.out.println("Your Vote Is Captured For NOTA | With VoterID " + voterID);
+							isVoted = false;
+						}
+					}
+					}
+				} while (isVoted);
+			} else {
+				System.out.println("Your Not Eligible For Voting. ( Gender Problem! )");
+			}
+			System.out.println("Thank You For Voting!!!");
+		}
+	}
+
+	public static void main(String[] args) {
+		castYourVote();
+	}
+}
--- labsessions/src/com/operators/TenaryOperatorDemo.java
+++ labsessions/src/com/operators/TenaryOperatorDemo.java
@@ -7,15 +7,6 @@ public class TenaryOperatorDemo {
 	int c = 750;
 
 	public void largestVar() {
-
-//		if (a > b && a > c) {
-//			System.out.println("A is Maximum");
-//		} else if (b > a && b > c) {
-//			System.out.println("B is Maximum");
-//		} else {
-//			System.out.println("C is Maximum");
-//		}
-
 		String msg = (a > b && a > c) ? "A is Maximum" : (b > a && b > c) ? "B is Maximum" : "C is Maximum";
 		System.out.println(msg);
 	}
